e3696a828da49e2d2386f0476091d966ce0c2f59
roquie <roquie0@gmail.com>
2017-03-07 22:36:34 +0300

    Initial commit after github initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # laravel-dusk-select2
 Select2.js support for the Laravel Dusk testing
+
+## Usage
+
+For default select2:
+
+```php
+$browse->select2('@selector');
+```
+
+If value not passed, it be selected automatically.
+
+Another way, if need concrete value:
+```php
+$browse->select2('@selector', 'you_text_value');
+```
+
+For multiple mode usage like this:
+```php
+$browse->select2('@selector', ['foo', 'bar'], 5);
+// the last parameter - count of seconds for ajax loading before choice item.
+```
+
+## Notice
+
+Css-selector for the `select` html tag should be ends with `+ select2` name.
+
+Example:
+
+We have a simple `select`:
+```html
+<select class="form-control select2-users" name="user_id">
+</select>
+```
+
+Our css-selector name: `.select2-users + .select2`
+
+## Todo
+
+ * [ ] Add Laravel Dusk tests for Laravel Dusk extend feature ;)
+ * [ ] Add gif demonstration how it works.
+ * [ ] May be use `+ select2` out of the box?.
+
+## License 
+
+MIT
---
+++ src/macro.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by Roquie.
+ * E-mail: roquie0@gmail.com
+ * GitHub: Roquie
+ * Date: 07/03/2017
+ */
+
+use Laravel\Dusk\Browser;
+
+/**
+ * Register simple macros for the Laravel Dusk.
+ * $field - selector, or @element
+ * $value - option value, may be multiple, eg. ['foo', 'bar']
+ * $wait  - count of seconds for ajax loading.
+ */
+Browser::macro('select2', function ($field, $value = null, $wait = 2) {
+    /** @var Browser $this */
+    $this->click($field);
+
+    // if $value equal null, find random element and click him.
+    // @todo: may be a couple of times move scroll to down (ajax paging)
+    if (null === $value) {
+        $this->script(join('', [
+            'var _dusk_s2_elements = jQuery.find(\'.select2-results__options .select2-results__option\');',
+            'var _dusk_s2_el = _dusk_s2_elements[Math.floor(Math.random()*(_dusk_s2_elements.length - 1))];',
+            '_dusk_s2_el.click();'
+        ]));
+
+        return $this;
+    }
+
+    // check if search field exists and fill it.
+    if ($element = $this->element('.select2-container .select2-search__field')) {
+        foreach ((array) $value as $item) {
+            $element->sendKeys($item);
+            sleep($wait);
+            $this->click('.select2-results__option--highlighted');
+            $element->clear();
+        }
+
+        return $this;
+    }
+
+    // another way - w/o search field.
+    $this->script("jQuery.find(\".select2-results__options .select2-results__option:contains('{$value}')\")[0].click()");
+
+    return $this;
+});
