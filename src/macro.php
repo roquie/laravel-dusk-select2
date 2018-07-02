f9c1bd926ce75d0bb9cedb85516188013e4ca0e1
andr <andrejs@gmail.com>
2018-07-03 01:54:43 +0300

    added support for select2 v3

--- src/macro.php
+++ src/macro.php
@@ -17,30 +17,61 @@ use Facebook\WebDriver\Exception\ElementNotVisibleException;
  */
 Browser::macro('select2', function ($field, $value = null, $wait = 2, $suffix = ' + .select2') {
     /** @var Browser $this */
-    $this->click($field . $suffix);
+    $element = $this->element($field.$suffix);
+    if ($element) {
+        // this is v4
+        $elementSelector = $field.$suffix;
+        $highlightedClass = '.select2-results__option--highlighted';
+        $highlightedSelector = '.select2-results__options '.$highlightedClass;
+        $selectableSelector = '.select2-results__options .select2-results__option';
+        $searchSelector = '.select2-container.select2-container--open .select2-search__field';
+    } else {
+        // try v3
+        $select = $this->element($field);
+        $selectId = null;
+
+        if (!$select || !($selectId = $select->getAttribute('id'))) {
+            // selects must have ids
+            return $this;
+        }
+        $elementSelector = '#s2id_'.$selectId;
+        if (!$this->element($elementSelector)) {
+            // give up
+            return $this;
+        }
+
+        $highlightedClass = 'select2-highlighted';
+        $highlightedSelector = '#select2-drop .'.$highlightedClass;
+        $selectableSelector = '#select2-drop .select2-result-selectable';
+        $searchSelector = '#select2-drop .select2-search:not(.select2-search-hidden) .select2-input, '
+            .$elementSelector.' .select2-input';
+    }
+
+
+    $this->click($elementSelector);
 
     // if $value equal null, find random element and click him.
     // @todo: may be a couple of times move scroll to down (ajax paging)
     if (null === $value) {
-        $this->waitFor('.select2-results__options .select2-results__option--highlighted');
-        $this->script(join('', [
-            "var _dusk_s2_elements = document.querySelectorAll('.select2-results__options .select2-results__option');",
-            "document.querySelector('.select2-results__options .select2-results__option--highlighted').classList.remove('select2-results__option--highlighted');",
+        $this->waitFor($highlightedSelector);
+        $this->script(implode('', [
+            "var _dusk_s2_elements = document.querySelectorAll('$selectableSelector');",
+            "document.querySelector('$highlightedSelector').classList.remove('$highlightedClass');",
             'var _dusk_s2_el = _dusk_s2_elements[Math.floor(Math.random()*(_dusk_s2_elements.length - 1))];',
-            "_dusk_s2_el.classList.add('select2-results__option--highlighted');"
+            "_dusk_s2_el.classList.add('$highlightedClass');"
         ]));
-        $this->click('.select2-results__option--highlighted');
+        $this->click($highlightedSelector);
 
         return $this;
     }
 
     // check if search field exists and fill it.
-    if ($element = $this->element('.select2-container.select2-container--open .select2-search__field')) {
+    if ($element = $this->element($searchSelector)) {
         try {
             foreach ((array) $value as $item) {
                 $element->sendKeys($item);
-                sleep($wait);
-                $this->click('.select2-results__option--highlighted');
+                $this->waitFor($highlightedSelector, $wait);
+                $this->click($highlightedSelector);
             }
 
             return $this;
@@ -50,7 +81,8 @@ Browser::macro('select2', function ($field, $value = null, $wait = 2, $suffix = 
     }
 
     // another way - w/o search field.
-    $this->script("jQuery.find(\".select2-results__options .select2-results__option:contains('{$value}')\")[0].click()");
+    $field = \str_replace('\\', '\\\\', $field);
+    $this->script("jQuery(\"$field\").val((function () { return jQuery(\"$field option:contains('$value')\").val(); })()).trigger('change')");
 
     return $this;
 });
--- tests/Browser/Select2Test.php
+++ tests/Browser/Select2Test.php
@@ -19,10 +19,33 @@ class Select2Test extends DuskTestCase
                 ->assertSeeIn('.js-example-basic-single + .select2', 'California')
                 ->select2('.js-example-basic-hide-search', 'Nev')
                 ->assertSeeIn('.js-example-basic-hide-search + .select2', 'Nevada')
+                ->click('h3:first-child') // close the previous select
                 ->select2('.js-example-basic-multiple', 'Wy')
                 ->waitFor('.select2-selection__choice')
-                ->select2('.js-example-basic-multiple')
+                ->click('h3:first-child') // close the previous select
                 ->select2('.js-data-example-ajax', 'laravel')
+                ->waitForText('laravel')
+            ;
+        });
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testShouldSupportSelect2v3()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit('/index_v3.html')
+                ->select2('#js-example-basic-single', 'Calif')
+                ->assertSeeIn('#s2id_js-example-basic-single', 'California')
+                ->select2('#js-example-basic-hide-search', 'Nev')
+                ->assertSeeIn('#s2id_js-example-basic-hide-search', 'Nevada')
+                ->click('h3:first-child') // close the previous select
+                ->select2('#js-example-basic-multiple', 'Wy')
+                ->waitFor('.select2-search-choice')
+                ->click('h3:first-child') // close the previous select
+                ->select2('#js-data-example-ajax', 'laravel')
                 ->waitForText('laravel')
             ;
         });
