4a25bbf44aa65e557a10b4519fb5dd8a3edadd94
roquie <roquie0@gmail.com>
2018-07-29 14:07:29 +0300

    Forgotten files.

--- tests/Browser/Select2Test.php
+++ tests/Browser/Select2Test.php
@@ -10,12 +10,16 @@ use Laravel\Dusk\Browser;
 
 class Select2Test extends DuskTestCase
 {
+    /**
+     * @throws \Throwable
+     */
     public function testShouldBeSelect2SelectableAutomatically()
     {
         $this->browse(function (Browser $browser) {
             $browser
                 ->visit('/')
                 ->select2('.js-example-basic-single', 'Calif')
+                ->assertSee('Select2 calling close event normally.') // https://github.com/roquie/laravel-dusk-select2/issues/12
                 ->assertSeeIn('.js-example-basic-single + .select2', 'California')
                 ->select2('.js-example-basic-hide-search', 'Nev')
                 ->assertSeeIn('.js-example-basic-hide-search + .select2', 'Nevada')
@@ -25,6 +29,7 @@ class Select2Test extends DuskTestCase
                 ->click('h3:first-child') // close the previous select
                 ->select2('.js-data-example-ajax', 'laravel')
                 ->waitForText('laravel')
+                ->assertSee('laravel/laravel')
             ;
         });
     }
@@ -47,6 +52,39 @@ class Select2Test extends DuskTestCase
                 ->click('h3:first-child') // close the previous select
                 ->select2('#js-data-example-ajax', 'laravel')
                 ->waitForText('laravel')
+                ->assertSee('laravel/laravel')
+            ;
+        });
+    }
+
+    /**
+     * @see https://github.com/roquie/laravel-dusk-select2/issues/12
+     * @throws Throwable
+     */
+    public function testShouldWorkWithSingleSelect2v4()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit('/single_v4.html')
+                ->select2('.js-example-basic-single', 'Calif')
+                ->assertSee('Select2 calling close event normally.')
+                ->assertSeeIn('.js-example-basic-single', 'California')
+            ;
+        });
+    }
+
+    /**
+     * @see https://github.com/roquie/laravel-dusk-select2/issues/12
+     * @throws Throwable
+     */
+    public function testShouldWorkWithSingleSelect2v3()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit('/single_v3.html')
+                ->select2('#js-example-basic-single', 'Calif')
+                ->assertSee('Select2 calling close event normally.')
+                ->assertSeeIn('#s2id_js-example-basic-single', 'California')
             ;
         });
     }
